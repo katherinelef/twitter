364b37909dec391e8bc68b88c54894726feebf61
kathy <katherine.lef@hotmail.com>
2017-11-14 18:19:46 -0500

    agregando contador version2

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 window.onload = function () {
 
-var form = document.getElementById("button")
+var form = document.getElementById("button");
 form.addEventListener("click", addText);
+
+var text = document.getElementById("text")
+text.addEventListener("keyup", addAccount);
 
 };
 
@@ -12,10 +15,18 @@ var input = document.getElementById("text").value;
 var message = document.createElement("div");
 var contenido = document.createTextNode(input);
 message.appendChild(contenido);
-
-message.classList.add ("message")
+message.classList.add ("message");
 
 var main = document.getElementById("box-image")
 main.appendChild(message);
+};
 
+function addAccount() {
+     var max = "150";
+     var message = document.getElementById("text").value;
+     var longitud = message.length;
+
+       if(longitud <= max) {
+            document.getElementById("contador").value = max-longitud;
+       }
 }
