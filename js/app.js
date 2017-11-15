c9c5f49c59b17279bb786edbf5cde13a048497a9
kathy <katherine.lef@hotmail.com>
2017-11-15 18:57:57 -0500

    añadiendo version 0.0.2 y hora

--- js/app.js
+++ js/app.js
@@ -1,32 +1,35 @@
 window.onload = function () {
-
-var form = document.getElementById("button");
-form.addEventListener("click", addText);
-
-var text = document.getElementById("text")
-text.addEventListener("keyup", addAccount);
-
+//evento para agregar el mensaje escrito//
+var button = document.getElementById("button");
+  button.addEventListener("click", addText);
+//evento para agregar contador de letras//
+var text = document.getElementById("text");
+  text.addEventListener("keyup", addAccount);
 };
 
+//creando funcion para addText//
 function addText() {
-var input = document.getElementById("text").value;
-/*document.getElementById("message").innerHTML = input;
-}*/
-var message = document.createElement("div");
-var contenido = document.createTextNode(input);
-message.appendChild(contenido);
-message.classList.add ("message");
-
-var main = document.getElementById("box-image")
-main.appendChild(message);
+  var length = document.getElementById("text").value.length;
+    if (length>"0" & length<="140"){
+      var input = document.getElementById("text").value;
+      var container = document.createElement("div");
+      var message= document.createElement("p");
+      var date = new Date();
+      var content = document.createTextNode(input +" "+ date.getHours()+":"+date.getMinutes());
+        container.appendChild(message);
+        message.appendChild(content);
+        message.classList.add ("message");
+      var main = document.getElementById("box-image")
+        main.appendChild(message);
+    }
 };
 
+//creando funcion para addAccount//
 function addAccount() {
-     var max = "150";
-     var message = document.getElementById("text").value;
-     var longitud = message.length;
-
-       if(longitud <= max) {
-            document.getElementById("contador").value = max-longitud;
+  var max = "140";
+  var message = document.getElementById("text").value;
+  var longitud = message.length;
+    if(longitud <= max) {
+      document.getElementById("contador").value = max-longitud;
        }
-}
+  };
