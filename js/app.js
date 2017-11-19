bd5dbcd2deb10bbed81b0a4e89ee8db946d521ae
kathy <katherine.lef@hotmail.com>
2017-11-18 21:41:22 -0500

    añadiendo version 0.0.3

--- js/app.js
+++ js/app.js
@@ -3,8 +3,11 @@ window.onload = function () {
 var button = document.getElementById("button");
   button.addEventListener("click", addText);
 //evento para agregar contador de letras//
+/*var text = document.getElementById("text");
+  text.addEventListener("keyup", addAccount);*/
+
 var text = document.getElementById("text");
-  text.addEventListener("keyup", addAccount);
+    text.addEventListener("keyup", addAccount);
 };
 
 //creando funcion para addText//
@@ -21,15 +24,31 @@ function addText() {
         message.classList.add ("message");
       var main = document.getElementById("box-image")
         main.appendChild(message);
-    }
-};
+        document.getElementById("text").value="";
+        document.getElementById("contador").value="140";
+        document.getElementById("contador").style.color="black";
+          }
+  };
 
-//creando funcion para addAccount//
-function addAccount() {
+function addAccount (){
   var max = "140";
   var message = document.getElementById("text").value;
   var longitud = message.length;
-    if(longitud <= max) {
-      document.getElementById("contador").value = max-longitud;
+  var contador=document.getElementById("contador");
+
+    if(longitud>="0" & longitud<"120") {
+      contador.style.color="black";
+      contador.value = max-longitud;
        }
-  };
+    else if(longitud>="120" & longitud<"130") {
+      contador.style.color="red";
+      contador.value = max-longitud;
+        }
+    else if(longitud>="130" & longitud<="140") {
+      contador.style.color="green";
+      contador.value = max-longitud;
+        }
+    else if(longitud>"140"){
+      contador.value =("-"+(longitud-max));
+          }
+}
